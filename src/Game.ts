2633117d65cde39dcd5784c79105351d4be114f4
Axel <axelskyttner@gmail.com>
2019-02-08 08:11:23 +0100

    moved game logic from dynamaker

--- src/Game.ts
+++ src/Game.ts
@@ -1,126 +1,222 @@
 export type GameState = {
-
-}
-type Player = {
-  bid: (info: GameState) => number,
-  id: number
-}
-
-type HistoryPoint = {
-  bid: Bid,
-  round: number
-}
-type Bid = {
-  amount: number,
+  currentBid: number,
   color: string,
-  playerId: number
+  history: Bid[],
+  myId: number
 }
-const MAX_GAME_ROUNDS = 10
+type Player = { bid: (info: GameState) => number, id: number }
+export type HistoryPoint = { bid: Bid, round: number }
+export type Bid = { amount: number, color: string, playerId: number }
+const MAX_GAME_ROUNDS = 50
 
-function getHighestBidAmount (bids: Bid[]) {
+export function generateGameState(options: any = {}): GameState {
+  return {
+      currentBid: options.currentBid !== undefined ? options.currentBid : 50,
+      color: options.color !== undefined ? options.color : 'RED',
+      history: options.history !== undefined ? options.history : [],
+      myId: options.myId !== undefined ? options.myBid : 0
+  }
+}
+
+export function generateBidPoint(options: any = {}): Bid {
+  return {
+      amount: options.amount !== undefined ? options.amount : 50,
+      color: options.color !== undefined ? options.color : 'RED',
+      playerId: options.playerId !== undefined ? options.playerId : []
+  }
+}
+
+function getHighestBidAmount(bids: Bid[]) {
   return Math.max(...bids.map(bid => bid.amount))
 }
 
-export function runBidGameRound(players: Player[], history: HistoryPoint[], gameRoundColor: string): Bid {   //ToDO: Create Player struct
-
+export function runBidGameRound(players: Player[], bidHistory: Bid[], gameRoundColor: string): Bid {
   const bids = players.reduce(function (bids: Bid[], player: Player) {
-    const currentBid = getHighestBidAmount(bids)
-    const result = player.bid({
-      currentBid,
-      color: gameRoundColor,
-      history
-    })
-    return bids.concat({ amount: result, playerId: player.id, color: gameRoundColor })
+      const currentBid = getHighestBidAmount(bids)
+      const playerInformation = { 
+          currentBid, 
+          color: gameRoundColor, 
+          myId: player.id, 
+          history: bidHistory }
+      const amount = player.bid(playerInformation)
+      const bidResult = { amount: amount, playerId: player.id, color: gameRoundColor }
+      return bids.concat(bidResult)
   }, [])
-
-  const winningBid = bids.reduce(function (bestBid: Bid, currentBid: Bid) {
-    if (playerCanAffordBid(currentBid.playerId, currentBid.amount, history)) {
-      return getHighestBid(bestBid, currentBid)
-    } else {
-      return bestBid
-    }
+  const truncatedBids = bids.map((bid) => {
+      return truncateBid(bid, bidHistory)
   })
+  const winningBid = truncatedBids.reduce(function (bestBid: Bid, currentBid: Bid) {
 
+      return getHighestBid(bestBid, currentBid)
+  })
   return winningBid
 }
 
+function shuffle(_a) {
+  const a = _a.slice()
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
+
 function getHighestBid(bid1: Bid, bid2: Bid) {
-  return (bid1.amount > bid2.amount) ?
-    bid1 :
-    bid2
+  return (bid1.amount > bid2.amount) ? bid1 : bid2
 }
 
+function isGameEnded(colors: String[]) {
+  const uniqueColors = Array.from(new Set(colors))
 
-function endConditionCheck(history: HistoryPoint[]) {
-  const winnings = {}
-  history.forEach(function (historyPoint) {
-    if (historyPoint.bid.color !== 'NONE') {
-      winnings[historyPoint.bid.color] = winnings[historyPoint.bid.color] === undefined ? 1 : winnings[historyPoint.bid.color] + 1
-    }
+  const nrColors = uniqueColors.map(color => {
+      return colors.filter(tmpColor => {
+          return tmpColor === color
+      }).length
   })
-
-  const keys = Object.keys(winnings)
-  let fiveEqual = false
-  keys.forEach(function (key) {
-    if (winnings[key] >= 5) {
-      fiveEqual = true
-    }
-  })
-
-  return fiveEqual
+  const fiveEquals = Math.max.apply(null, nrColors) >= 5
+  return fiveEquals
 }
 
+function getMoneyLeft(playerId: Number, history: Bid[]) {
+  const bids = history.filter(function (item: Bid) { return item.playerId === playerId })
+  const sum = bids.reduce((sum: number, item: Bid) => sum + item.amount,
+      0)
+  return 100 - sum
+}
 
-function playerCanAffordBid(playerId: number, playerBid: number, history) {
+function truncateBid(bid: Bid, history: Bid[]): Bid {
+  const moneyLeft = getMoneyLeft(bid.playerId, history)
+  if (moneyLeft < bid.amount) {
+      return {
+          playerId: bid.playerId,
+          amount: moneyLeft,
+          color: bid.color
+      }
+  } else {
+      return bid
+  }
+}
 
-  const bids = history.filter(function (item: Bid) {
-    return item.playerId === playerId
-  })
-
+function playerCanAffordBid(playerId: number, playerBid: number, history: Bid[]) {
+  const bids = history.filter(function (item: Bid) { return item.playerId === playerId })
   const sum = bids.reduce((sum: number, item: Bid) => sum + item.amount, playerBid)
-
   return sum <= 100
 }
-
 
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-
 export function newGame(players: Player[]) {
-
-  let history = []
-
   const colorList = ["RED", "BLUE", "GREEN", "YELLOW"]
+
+  const gameColors = []
   for (let i = 0; i < MAX_GAME_ROUNDS; i++) {
-    const gameRoundColor = colorList[randomIntFromInterval(0, colorList.length - 1)]
-    const winningBid = runBidGameRound(players, history, gameRoundColor)
-    const something = { playerId: winningBid.playerId, bid: winningBid.amount, winnings: gameRoundColor, round: i }   //ToDO Create HistoryStruct
-
-    history = history.concat(something)
-    const endOfGame = endConditionCheck(history)
-    if (endOfGame) {
-      break
-    }
+      const gameRoundColor = colorList[randomIntFromInterval(0, colorList.length - 1)]
+      gameColors.push(gameRoundColor)
+      const endOfGame = isGameEnded(gameColors)
+      if (endOfGame) {
+          break
+      }
   }
+  const history = gameColors.reduce((history, gameRoundColor) => {
+      const shuffledPlayers = shuffle(players)
+      const winningBid = runBidGameRound(shuffledPlayers, history, gameRoundColor)
+      return history.concat(winningBid)
+  }, [])
 
-  const winnings = {}
-  history.forEach(function (item: Bid) {
-    if (item.color !== 'NONE') {
-      const playerScore = winnings[item.playerId] || {}
-      playerScore[item.color] = playerScore[item.color] === undefined ? 1 : playerScore[item.color] + 1
-      winnings[item.playerId] = playerScore
-    }
+  return { history: history, winner: getWinner(history) }
+}
+
+function sum(arr) {
+  return arr.reduce((val1, val2) => {
+      return val1 + val2
+  })
+}
+
+function getUniquePlayerIds(bids: Bid[]) {
+  const playerIds = bids.map(point => point.playerId)
+  return Array.from(new Set(playerIds))
+}
+function getUniqueColors(bids: Bid[]) {
+  const playerIds = bids.map(point => point.color)
+  return Array.from(new Set(playerIds))
+}
+
+function getColorScore(uniqueColors: string[], bids: Bid[]) {
+  const scoreList = [30, 15, 5, 0]
+
+  const nrColors = uniqueColors.map((color) => {
+      return bids.filter(bid => bid.color === color).length
   })
 
+  const values = nrColors.slice().sort((a, b) => {
+      return b - a
+  })
+
+  const colorScore = uniqueColors.reduce((colorScore, color, index) => {
+      const nrSamples = nrColors[index]
+      const nrSamplesIndex = values.indexOf(nrSamples);
+      const score = scoreList[nrSamplesIndex]
+
+      colorScore[color] = score
+      return colorScore
+  }, {})
+  return colorScore
+}
+
+function getPlayerWithHighestScore(playerIds: number[], scores: number[]): number {
+  const highestScoreIndex = scores.indexOf(Math.max.apply(null, scores))
+  return playerIds[highestScoreIndex]
+}
+
+function getPlayerScores(playerIds: Number[], history: Bid[]) {
+  const uniqueColors = getUniqueColors(history)
+  const colorScore = getColorScore(uniqueColors, history)
+  const playerScores = playerIds.map(playerId => {
+      const relevantGames = history.filter(historyPoint => {
+          return historyPoint.playerId === playerId
+      })
+
+      return sum(relevantGames.map(bid => {
+          return colorScore[bid.color]
+      }))
+  })
+  return playerScores
+}
+function getWinner(history: Bid[]) {
+  if (history.length === 0) {
+      throw new Error('No winner if empty history!')
+  }
+  const uniquePlayers = getUniquePlayerIds(history)
+  const playerScores = getPlayerScores(uniquePlayers, history)
+  const winningPlayerId = getPlayerWithHighestScore(uniquePlayers, playerScores)
+  return winningPlayerId
+}
+
+const playerGenerator = function (id, bidFunction) {
   return {
-    winnings: winnings,
-    history: history
+      id: id,
+      bid: bidFunction
   }
 }
 
 export const __test__ = {
   playerCanAffordBid,
-  runBidGameRound
+  runBidGameRound,
+  getWinner,
+  isGameEnded
+}
+
+export const functions = {
+  isGameEnded,
+  runBidGameRound,
+  playerCanAffordBid,
+  getWinner,
+  playerGenerator,
+  getPlayerScores,
+  getMoneyLeft
 }
--- tests/game.spec.ts
+++ tests/game.spec.ts
@@ -1,40 +1,47 @@
-import { __test__, newGame } from '../src/Game'
-const { expect } = require('chai')
-const playerGenerator = function (id, bidFunction) {
+import {expect}  from 'chai'
+import { 
+  newGame, 
+  functions, 
+  HistoryPoint,Bid } from '../src/Game'
 
-  return {
-    id: id,
-    bid: bidFunction
-}
-}
 
-describe ("bid functions", function () {
-  it("a player that bids above should win" , function () {
-      const player1 = playerGenerator(1, () => {
-        return 1
-      })
-      const player2 = playerGenerator(2, () => {
-        return 2
-      })
+const { playerGenerator, isGameEnded, getWinner } = functions
 
-      const {winnings, history} = newGame([player1, player2])
-      console.log('history', history)
-      console.log("winnings", winnings)
-      expect(Object.keys(winnings)).to.contain('2')
-  })
-  it('a player that bids to high should not win', () => {
+describe("newGame should return the winner", () => {
+  it("should declare the one with valid bids the winner", () => {
     const player1 = playerGenerator(1, () => {
       return 1
     })
     const player2 = playerGenerator(3, () => {
       return 1001
     })
-
-    const {winnings, history} = newGame([player1, player2])
-    console.log('winnings', winnings)
-    console.log('history', history)
-    expect(Object.keys(winnings)).to.not.contain('3')
-
+    const { history, winner } = newGame([player1, player2])
+    expect(winner).to.equal(1)
+  })
+  it("should declare the one with highest bids the winner", () => {
+    const player1 = playerGenerator(1, () => {
+      return 1
+    })
+    const player2 = playerGenerator(3, () => {
+      return 2
+    })
+    const { history, winner } = newGame([player1, player2])
+    expect(winner).to.equal(3)
+  })
+  it("should have 5 equal of any color when game is over", () => {
+    const player1 = playerGenerator(1, () => {
+      return 1
+    })
+    const player2 = playerGenerator(3, () => {
+      return 2
+    })
+    const { history, winner } = newGame([player1, player2])
+    const yellowEntries = history.filter(entry => entry.color === 'YELLOW').length
+    const blue = history.filter(entry => entry.color === 'BLUE').length
+    const red = history.filter(entry => entry.color === 'RED').length
+    const green = history.filter(entry => entry.color === 'GREEN').length
+    const max = Math.max.apply(null, [yellowEntries, blue, red, green])
+    expect(max).to.equal(5)
   })
 })
 
@@ -56,8 +63,8 @@ describe('player interface', () => {
 
 
 
-describe('__test__ functions', () => {
-  const {playerCanAffordBid, runBidGameRound} = __test__
+describe('help functions', () => {
+  const { playerCanAffordBid, runBidGameRound } = functions
 
   describe('runBidGameRound', () => {
     it('', () => {
@@ -72,7 +79,40 @@ describe('__test__ functions', () => {
       const history = []
 
       const result = runBidGameRound(players, history, color)
-      expect(result.playerId).to.equal(1)
+      expect(result.playerId).to.equal(3)
+      expect(result.amount).to.equal(100)
+    })
+  })
+
+  function generateHistoryPoint (options: any = {}) : Bid {
+    return {
+            color: options.color || 'BLUE',
+            playerId: options.playerId || 0,
+            amount: options.amount !== undefined ? options.amount : 1
+        }
+  }
+  describe("getWinner", function () {
+    it("should return winner if onlmy one player present", function () {
+
+      const historyList: Bid[] = [
+        generateHistoryPoint({playerId: 1})
+      ]
+      const winner = getWinner(historyList)
+      expect(winner).to.equal(1)
+    })
+
+    it("should return winner to the one with two blues vs 2 green", function () {
+      
+      const historyList: Bid[] = [
+        generateHistoryPoint({playerId: 1, color: 'green'}),
+        generateHistoryPoint({playerId: 1, color: 'blue'}),
+        generateHistoryPoint({playerId: 3, color: 'green'}),
+        generateHistoryPoint({playerId: 3, color: 'blue'}),
+        generateHistoryPoint({playerId: 2, color: 'blue'}),
+        generateHistoryPoint({playerId: 2, color: 'blue'})
+      ]
+      const winner = getWinner(historyList)
+      expect(winner).to.equal(2)
     })
   })
   describe('playerCanAffordBid', () => {
@@ -85,9 +125,10 @@ describe('__test__ functions', () => {
     it('should not afford if bid is under 100 but with history', () => {
       const playerId = 1
       const playerBid = 20
-      const history = [{
-        playerId: 1, bid: 90
-      }]
+      const history = [
+        generateHistoryPoint({amount: 90, playerId: 1, bid: 90
+        })
+      ]
       expect(playerCanAffordBid(playerId, playerBid, history)).to.equal(false)
     })
     it('should not afford if bid is above 100 and no history', () => {
@@ -97,4 +138,31 @@ describe('__test__ functions', () => {
       expect(playerCanAffordBid(playerId, playerBid, history)).to.equal(false)
     })
   })
+
+  describe('isGameEnded', () => {
+    it('should returnt true when 5 elements are of same color', ()=> {
+      const history = [
+        'BLUE',
+        'BLUE',
+        'BLUE',
+        'BLUE',
+        'BLUE'
+      ]
+      expect(isGameEnded(history)).to.equal(true)
+    })
+     it('should return false when 4 elements are of same color', ()=> {
+      const history = [
+        'RED',
+        'RED',
+        'RED',
+        'RED'
+      ]
+      expect(isGameEnded(history)).to.equal(false)
+    })
+  })
 })
+
+
+
+
+export { }
